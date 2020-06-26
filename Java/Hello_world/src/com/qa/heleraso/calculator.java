9637fe31cd6bec8648586a2729582513aedd39ea
gianluca-fernandez-QA <gianluca.fernandez@academytrainee.com>
2020-06-26 10:06:29 +0100

    Updated Java Project for multiple methods

--- Java/Hello_world/src/com/qa/heleraso/calculator.java
+++ Java/Hello_world/src/com/qa/heleraso/calculator.java
@@ -1,5 +1,6 @@
 package com.qa.heleraso;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class calculator {
@@ -7,52 +8,158 @@ public class calculator {
 
 	public static void main(String[] args) {
 //		 TODO Auto-generated method stub
-		System.out.print("Enter string ");
-		String cheString1 =  myObj.nextLine();
-		System.out.println(cheString1);
-		System.out.println(stringCheck(cheString1));
-		System.out.print("Pick +, -, /, * ");
-		String cond = myObj.nextLine();
-		System.out.println(calcuMath(cond));
+		Boolean selectMethod = false;
+		System.out.println("Select A Valid Method: {1 calcuMath} {2 stringCheck} {3 loopNum} {4 helloWorld} {5 Exit}");
+		System.out.println("Or type Exit to end program");
 		
+		methodPicker(selectMethod);
+
+	}
+	public static void methodPicker(Boolean meth) {
+		int test = myObj.nextInt();
+		while (meth = false) {
+			switch (test) {
+			case 1:
+				calcuMath();
+				break;
+			case 2:
+				stringCheck();
+				break;
+			case 3:
+				loopNum();
+				break;
+			case 4:
+				System.out.println(Hello_world());
+				break;
+			case 5:
+				meth = true;
+				break;
+			default:
+				System.out.println("ERROR: No methods entered ");
+				break;
+			}
+
+		}
 	}
 
-	public static double calcuMath(String gatto) {
+	public static String Hello_world() {
+		String Hewwo_world = "Hello World!";
+		return Hewwo_world;
+	}
+
+	public static void calcuMath() {
+		System.out.print("Pick +, -, /, * ");
+		String cond = myObj.nextLine();
 		System.out.print("First Num ");
 		double firstNum = myObj.nextDouble();
 		System.out.print("Second Num ");
 		double secondNum = myObj.nextDouble();
 
-		switch (gatto) {
+		switch (cond) {
 		case "+":
-			return (firstNum + secondNum);
+			System.out.println(firstNum + secondNum);
 
 		case "-":
-			return (firstNum - secondNum);
+			System.out.println(firstNum - secondNum);
 
 		case "/":
-			return (firstNum / secondNum);
+			System.out.println(firstNum / secondNum);
 
 		case "*":
-			return (firstNum * secondNum);
+			System.out.println(firstNum * secondNum);
 
 		default:
 			System.out.println("Enter a conditional");
 			break;
 
 		}
-		return 0;
 
 	}
 
-	public static String stringCheck(String checker) {
-
-		String cheString2 = "Hello World";
-		if (cheString2.equals(checker)) {
-			return "These match";
+	public static void stringCheck() {
+		System.out.print("Enter First string ");
+		String cheString1 = myObj.nextLine();
+		System.out.print("Enter Second string ");
+		String cheString2 = myObj.nextLine();
+		if (cheString2.equals(cheString1)) {
+			System.out.println("These match");
 		} else {
-			return "These don't match";
+			System.out.println("These don't match");
 		}
 	}
+
+	public static void loopNum() {
+		System.out.println("0 for Numbers, 1 For string");
+		int numOrString = myObj.nextInt();
+		System.out.println("First Number to loop");
+		int loopNum1 = myObj.nextInt();
+		System.out.println("Second Number to loop");
+		int loopNum2 = myObj.nextInt();
+		int i = 0;
+		int arrayCalculations = (loopNum2 - loopNum1) + 1;
+		if (numOrString == 0) {
+			while (loopNum1 <= (loopNum2 - 1)) {
+				System.out.println(loopNum1);
+				int theArray[] = new int[arrayCalculations];
+				theArray[i] = loopNum1;
+				ArrayList<Integer> theArrayList = new ArrayList<>();
+				theArrayList.add(loopNum1);
+				System.out.println(theArray[i] * 10);
+				System.out.println(theArrayList);
+				loopNum1++;
+				i++;
+
+			}
+		} else {
+			while (loopNum1 <= loopNum2) {
+				System.out.println(convertNumber(loopNum1));
+				loopNum1++;
+
+			}
+		}
+
+	}
+
+	private static final String[] units = { "", " one", " two", " three", " four", " five", " six", " seven", " eight",
+			" nine" };
+	private static final String[] twoDigits = { " ten", " eleven", " twelve", " thirteen", " fourteen", " fifteen",
+			" sixteen", " seventeen", " eighteen", " nineteen" };
+	private static final String[] tenMultiples = { "", "", " twenty", " thirty", " forty", " fifty", " sixty",
+			" seventy", " eighty", " ninety" };
+	private static final String[] placeValues = { " ", " thousand", " million", " billion", " trillion" };
+
+	private static String convertNumber(long number) {
+		String word = "";
+		int index = 0;
+		do {
+			// take 3 digits in each iteration
+			int num = (int) (number % 1000);
+			if (num != 0) {
+				String str = ConversionForUptoThreeDigits(num);
+				word = str + placeValues[index] + word;
+			}
+			index++;
+			// next 3 digits
+			number = number / 1000;
+		} while (number > 0);
+		return word;
+	}
+
+	private static String ConversionForUptoThreeDigits(int number) {
+		String word = "";
+		int num = number % 100;
+		if (num < 10) {
+			word = word + units[num];
+		} else if (num < 20) {
+			word = word + twoDigits[num % 10];
+		} else {
+			word = tenMultiples[num / 10] + units[num % 10];
+		}
+
+		word = (number / 100 > 0) ? units[number / 100] + " hundred" + word : word;
+		return word;
+	}
+	// Sourced From
+	// https://www.netjstech.com/2018/11/converting-numbers-to-words-java-program.html
 
 }
